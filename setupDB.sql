5c4f542e7509716c2ef395f6418ad133c246c0ef
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-03-19 23:35:23 -0500

    database design complete.

---
+++ setupDB.sql
@@ -0,0 +1,34 @@
+USE fog;
+DROP TABLE dhcpSubnets;
+CREATE TABLE dhcpSubnets(
+dsID int NOT NULL AUTO_INCREMENT,
+dsSubnet VARCHAR(50) NOT NULL UNIQUE,
+dsNetmask VARCHAR(50) NOT NULL,
+dsOptionSubnetMask VARCHAR(50),
+dsRangeDynamicBootp VARCHAR(100),
+dsDefaultLeaseTime int,
+dsMaxLeaseTime int,
+dsOptionRouters VARCHAR(50),
+dsOptionDomainNameServers VARCHAR(255),
+dsNextServer VARCHAR(50),
+PRIMARY KEY (dsID)
+);
+
+CREATE TABLE dhcpReservations(
+drID int NOT NULL AUTO_INCREMENT,
+dr_hmID int NOT NULL,
+dr_dsID int NOT NULL,
+drFileName VARCHAR(255),
+drIP VARCHAR(50) UNIQUE,
+PRIMARY KEY (drID)
+);
+
+CREATE TABLE dhcpClasses(
+dcID int NOT NULL AUTO_INCREMENT,
+dc_dsID int NOT NULL,
+dcClass VARCHAR(255),
+dcMatch VARCHAR(255),
+dcMatchOption VARCHAR(255),
+PRIMARY KEY (dcID)
+);
+
