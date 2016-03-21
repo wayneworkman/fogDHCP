6aaccc5ddba37f0c09a233f25b775b638efe2b5a
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-03-21 12:18:38 -0500

    Changed the structure of the DB to be more flexible and work independent of FOG.

--- setupDB.sql
+++ setupDB.sql
@@ -1,7 +1,7 @@
 USE fog;
 CREATE TABLE IF NOT EXISTS dhcpSubnets(
 dsID int NOT NULL AUTO_INCREMENT,
-dsSubnet VARCHAR(50) NOT NULL UNIQUE,
+dsSubnet VARCHAR(50) NOT NULL,
 dsNetmask VARCHAR(50) NOT NULL,
 dsOptionSubnetMask VARCHAR(50),
 dsRangeDynamicBootpStart VARCHAR(50),
@@ -10,22 +10,37 @@ dsDefaultLeaseTime int,
 dsMaxLeaseTime int,
 dsOptionRouters VARCHAR(50),
 dsOptionDomainNameServers VARCHAR(255),
+dsOptionNtpServers VARCHAR(255),
 dsNextServer VARCHAR(50),
+dsCustomArea1 VARCHAR(255),
+dsCustomArea2 VARCHAR(255),
 PRIMARY KEY (dsID)
-);
-CREATE TABLE IF NOT EXISTS dhcpReservations(
-drID int NOT NULL AUTO_INCREMENT,
-dr_hmID int NOT NULL,
-dr_dsID int NOT NULL,
-drFileName VARCHAR(255),
-drIP VARCHAR(50) UNIQUE,
-PRIMARY KEY (drID)
 );
 CREATE TABLE IF NOT EXISTS dhcpClasses(
 dcID int NOT NULL AUTO_INCREMENT,
 dc_dsID int NOT NULL,
 dcClass VARCHAR(255),
 dcMatch VARCHAR(255),
-dcMatchOption VARCHAR(255),
+dcMatchOption1 VARCHAR(255),
+dcMatchOption2 VARCHAR(255),
+dcMatchOption3 VARCHAR(255),
 PRIMARY KEY (dcID)
+);
+CREATE TABLE IF NOT EXISTS dhcpFilenames(
+dfID int NOT NULL AUTO_INCREMENT,
+dfFileName VARCHAR(255) NOT NULL UNIQUE,
+dfDescription VARCHAR(255),
+PRIMARY KEY (dfID)
+);
+CREATE TABLE IF NOT EXISTS dhcpReservations(
+drID int NOT NULL AUTO_INCREMENT,
+drMAC VARCHAR(17) NOT NULL UNIQUE,
+drName VARCHAR(16) NOT NULL UNIQUE,
+drFileName VARCHAR(255),
+drIP VARCHAR(50) UNIQUE,
+drOptionDomainNameServers VARCHAR(255),
+drCustomArea1 VARCHAR(255),
+drCustomArea2 VARCHAR(255),
+drCustomArea3 VARCHAR(255),
+PRIMARY KEY (drID)
 );
