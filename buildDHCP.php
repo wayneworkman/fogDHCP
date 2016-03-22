059fa3c56acbb723a40ae7298a4a7f704ab25a10
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-03-22 14:10:36 -0500

    DHCP conf building engine is almost done.

--- buildDHCP.php
+++ buildDHCP.php
@@ -8,8 +8,8 @@ $DHCP_Service_Sleep_Time=0;
 $DHCP_To_Use="";
 $Current_DHCP_Checksum="";
 $New_DHCP_Checksum="";
-
-
+$New_File="";
+$New_Line="\n";
 
 
 //start loop.
@@ -53,8 +53,15 @@ while(1) {
 
 
 	//Build Global Options into file.
+	$sql = "SELECT dgOption FROM dhcpGlobals ORDER BY dgID ASC";
+        $result = $link->query($sql);
+        if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                        $dgOption = $row["dgOption"];
+			$New_File = $New_File . $dgOption . $New_Line;
+                }
+        }
 	
-		//select Data.
 
 
 
@@ -62,44 +69,149 @@ while(1) {
 
 
 	//Build Subnets.
-		//Build the subnet's classes.
-		//select data.
+	$sql = "SELECT * FROM dhcpSubnets ORDER BY dsID ASC";
+        $result = $link->query($sql);
+        if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+			$dsID = $row["dsID"];
+                        $dsSubnet = $row["dsSubnet"];
+			$dsNetmask = $row["dsNetmask"];
+			$dsOptionSubnetMask = $row["dsOptionSubnetMask"];
+			$dsRangeDynamicBootpStart = $row["dsRangeDynamicBootpStart"];
+			$dsRangeDynamicBootpEnd = $row["dsRangeDynamicBootpEnd"];
+			$dsDefaultLeaseTime = $row["dsDefaultLeaseTime"];
+			$dsMaxLeaseTime = $row["dsMaxLeaseTime"];
+			$dsOptionRouters = $row["dsOptionRouters"];
+			$dsOptionDomainNameServers = $row["dsOptionDomainNameServers"];
+			$dsOptionNtpServers = $row["dsOptionNtpServers"];
+			$dsNextServer = $row["dsNextServer"];
+			$dsCustomArea1 = $row["dsCustomArea1"];
+			$dsCustomArea2 = $row["dsCustomArea2"];
+			$dsCustomArea3 = $row["dsCustomArea3"];
+			
+			
+			$New_File = $New_File . "subnet " . $dsSubnet . " netmask " . $dsNetmask . "{" . $New_Line;
 
+			if ($dsOptionSubnetMask != "") {
+				$New_File = $New_File . "option subnet-mask " . $dsOptionSubnetMask . ";" . $New_Line;
+			}
+			if ($dsRangeDynamicBootpStart != "" && dsRangeDynamicBootpEnd != "") {
+				$New_File = $New_File . "range dynamic-bootp " . $dsRangeDynamicBootpStart . " " . $dsRangeDynamicBootpEnd ";" . $New_Line;
+			}
+			if ($dsDefaultLeaseTime != "") {
+				$New_File = $New_File . "default-lease-time " . $dsDefaultLeaseTime . ";" . $New_Line;
+                        }
+			if ($dsMaxLeaseTime != "") {
+				$New_File = $New_File . "max-lease-time " . $dsMaxLeaseTime . ";" . $New_Line;
+                        }
+			if ($dsOptionRouters != "") {
+				$New_File = $New_File . "option routers " . $dsOptionRouters . ";" . $New_Line;
+                        }
+			if ($dsOptionDomainNameServers != "") {
+				$New_File = $New_File . "option domain-name-servers " . $dsOptionDomainNameServers . ";" . $New_Line;
+                        }
+			if ($dsOptionNtpServers != "") {
+				$New_File = $New_File . "option ntp-servers " . $dsOptionNtpServers . ";" . $New_Line;
+                        }
+			if ($dsNextServer != "") {
+				$New_File = $New_File . "next-server " . $dsNextServer . ";" . $New_Line;
+                        }
+			if ($dsCustomArea1 != "") {
+				$New_File = $New_File . $dsCustomArea1 . $New_Line;
+                        }
+			if ($dsCustomArea2 != "") {
+				$New_File = $New_File . $dsCustomArea2 . $New_Line;
+                        }
+			if ($dsRangeDynamicBootpStart != "") {
+				$New_File = $New_File . $dsCustomArea3 . $New_Line;
+                        }
 
-
+			//Build classes for this subnet.	
+			$sql = "SELECT * FROM dhcpClasses WHERE dc_dsID = $dsID ORDER BY dcID ASC";
+        		$result2 = $link->query($sql);
+        		if ($result2->num_rows > 0) {
+                		while($row2 = $result2->fetch_assoc()) {
+					$dcClass = $row2["dcClass"];
+					$dcMatch = $row2["dcMatch"];
+					$dcMatchOption1 = $row2["dcMatchOption1"];
+					$dcMatchOption2 = $row2["dcMatchOption2"];
+					$dcMatchOption3 = $row2["dcMatchOption3"];
+					$New_File = $New_File . "    class \"" . $dcClass . "\" {" . $New_Line;
+					$New_File = $New_File . "        " . $dcMatch . $New_Line;
+					$New_File = $New_File . "        " . $dcMatchOption1 . $New_Line;
+					if ($dcMatchOption2 != "") {
+						$New_File = $New_File . "        " . $dcMatchOption2 . $New_Line;
+					}
+					if ($dcMatchOption3 != "") {
+                                                $New_File = $New_File . "        " . $dcMatchOption3 . $New_Line;
+                                        }
+					$New_File = $New_File . "    }" . $New_Line;
+				}
+			}
+		$New_File = $New_File . "}" . $New_Line;
+		}
+	}
 
 
 
 	//Build Reservations.
-		//select data.
+	$sql = "SELECT * FROM dhcpReservations ORDER BY drID ASC";
+	$result = $link->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+
+			$drMAC = $row["drMAC"];
+			$drName = $row["drName"];
+			$drFilename = $row["drFilename"];
+			$drIP = $row["drIP"];
+			$drOptionDomainNameServers = $row["drOptionDomainNameServers"];
+			$drCustomArea1 = $row["drCustomArea1"];
+			$drCustomArea2 = $row["drCustomArea2"];
+			$drCustomArea3 = $row["drCustomArea3"];
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+			$New_File = $New_File . "host " . $drName . " {" . $New_Line;
+			$New_File = $New_File . "    hardware ethernet " . $drMAC . ";" . $New_Line;
+			
+			if ($drFileName != "") {
+				$New_File = $New_File . "    filename \"" . $drFilename . "\";" . $New_Line;
+			}
+			if ($drIP != "") {
+                                $New_File = $New_File . "    fixed-address " . $drIP . ";" . $New_Line;
+                        }
+			if ($drOptionDomainNameServers != "") {
+                                $New_File = $New_File . "    option domain-name-servers " . $drFilename . ";" . $New_Line;
+                        }
+			if ($drCustomArea1 != "") {
+                                $New_File = $New_File . "    " . $drCustomArea1 . $New_Line;
+                        }
+			if ($drCustomArea2 != "") {
+                                $New_File = $New_File . "    " . $drCustomArea2 . $New_Line;
+                        }
+			if ($drCustomArea3 != "") {
+                                $New_File = $New_File . "    " . $drCustomArea3 . $New_Line;
+                        }
+		}
+	}
 
 
 
 
 
 	// Write commands for this user, for this setting, to the setting's file.
-	$file = $PathToSMBShares . $JaneSettingsNickName . ".ps1";
+	$file = $DHCP_To_Use . ".new";
 	if (file_exists($file)) {
-		$current = file_get_contents($file);
-		$current = $current . $ThisCOMMAND;	
-	} else {
-		$current = $ThisCOMMAND;
+		unlink($file);
 	}
-	file_put_contents($file, $current);
+	file_put_contents($file, $New_File);
+
+
+
+	//Check MD5 Sum.
+
+
+
+
 
 
 //end loop.
