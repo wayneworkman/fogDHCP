85b63c81601e3450fc748c2ecb0511f31a5b66e6
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-03-22 11:53:35 -0500

    Started work on the engine.

--- buildDHCP.php
+++ buildDHCP.php
@@ -1,5 +1,77 @@
 <?php
 
+$servername="";
+$username="";
+$password="";
+$database="fog";
+$DHCP_Service_Sleep_Time=0;
+$DHCP_To_Use="";
+$Current_DHCP_Checksum="";
+$New_DHCP_Checksum="";
+
+
+
+
+//start loop.
+while(1) {
+	
+	// Sleep.
+	sleep($DHCP_Service_Sleep_Time);
+
+
+	// Create connection
+	$link = new mysqli($servername, $username, $password, $database);
+	// Check connection
+	if ($link->connect_error) {
+        	// Couldn't establish a connection with the database.
+        	die("Error");
+	}
+
+
+
+	//Get sleep time.
+	$sql = "SELECT settingValue FROM globalSettings WHERE settingKey = 'DHCP_Service_Sleep_Time' LIMIT 1";
+	$result = $link->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$DHCP_Service_Sleep_Time = $row["settingValue"];
+		}
+	}
+	
+
+	//Get DHCP Config file.
+	$sql = "SELECT settingValue FROM globalSettings WHERE settingKey = 'DHCP_To_Use' LIMIT 1";
+        $result = $link->query($sql);
+        if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                        $DHCP_To_Use = $row["settingValue"];
+                }
+        }
+
+
+
+
+
+	//Build Global Options into file.
+	
+		//select Data.
+
+
+
+
+
+
+	//Build Subnets.
+		//Build the subnet's classes.
+		//select data.
+
+
+
+
+
+
+	//Build Reservations.
+		//select data.
 
 
 
@@ -7,13 +79,29 @@
 
 
 
-// Write commands for this user, for this setting, to the setting's file.
-$file = $PathToSMBShares . $JaneSettingsNickName . ".ps1";
-if (file_exists($file)) {
-	$current = file_get_contents($file);
-	$current = $current . $ThisCOMMAND;	
-} else {
-	$current = $ThisCOMMAND;
+
+
+
+
+
+
+
+
+
+
+
+
+	// Write commands for this user, for this setting, to the setting's file.
+	$file = $PathToSMBShares . $JaneSettingsNickName . ".ps1";
+	if (file_exists($file)) {
+		$current = file_get_contents($file);
+		$current = $current . $ThisCOMMAND;	
+	} else {
+		$current = $ThisCOMMAND;
+	}
+	file_put_contents($file, $current);
+
+
+//end loop.
 }
-file_put_contents($file, $current);
 ?>
